3c12f7acfa26ebbe174dee4a306d964fe0c270c4
KalinVasilevSV <kalinsv@abv.bg>
2023-04-19 19:02:09 +0300

    web.xml

--- src/main/java/org/studyprojects/examsystem/home/HomeController.java
+++ src/main/java/org/studyprojects/examsystem/home/HomeController.java
@@ -1,7 +1,16 @@
 package org.studyprojects.examsystem.home;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class HomeController {
+
+    @RequestMapping("/home")
+    @ResponseBody
+    public String home(){
+        return "home cotroller reporting";
+    }
 }
