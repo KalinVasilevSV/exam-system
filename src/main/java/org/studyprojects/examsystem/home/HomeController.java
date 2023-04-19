a97cc0ba759a10353bd431d6754bb7878c7a3a48
KalinVasilevSV <kalinsv@abv.bg>
2023-04-19 21:03:06 +0300

    add root redirect to home

--- src/main/java/org/studyprojects/examsystem/home/HomeController.java
+++ src/main/java/org/studyprojects/examsystem/home/HomeController.java
@@ -3,9 +3,15 @@ package org.studyprojects.examsystem.home;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class HomeController {
+    @RequestMapping("/")
+    public RedirectView homeredirect(){
+        return new RedirectView("/home",true);
+    }
+
     @RequestMapping("/home")
     @ResponseBody
     public String home(){
