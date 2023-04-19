776b0ec2924bd7c183fc6dd0b3db41d63ef4a9a6
KalinVasilevSV <kalinsv@abv.bg>
2023-04-19 19:32:43 +0300

    web.xml working

--- src/main/java/org/studyprojects/examsystem/error/ErrorController.java
+++ src/main/java/org/studyprojects/examsystem/error/ErrorController.java
@@ -1,6 +1,7 @@
 package org.studyprojects.examsystem.error;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
@@ -11,6 +12,7 @@ import java.util.Map;
 public class ErrorController {
 
     @RequestMapping("/error")
+    @ResponseBody
     public Map<String,Object> handle(HttpServletRequest request){
         Map<String,Object> map = new HashMap<>();
         map.put("status",request.getAttribute("jakarta.servlet.error.status_code"));
